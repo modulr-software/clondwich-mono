12006d0eb61cadf2bcfa69e22c213b3db8b6bae1
tttTAAAKUUU <tctembedza@gmail.com>
2025-04-22 09:42:23 +0200

    adding routes

---
+++ backend/src/routes.clj
@@ -0,0 +1,18 @@
+(ns sandwich-app.routes
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.util.response :refer [response]]
+            [db.core :as db]))
+
+(defroutes app-routes
+  (GET "/" [] (response "Welcome to the Sandwich App!"))
+
+  (GET "/images" []
+    (response (db/get-images-by-vote-count)))
+
+  (POST "/vote" [image_id vote]
+    (db/insert-vote (Integer/parseInt image_id)
+                    (= vote "true"))
+    (response {:status "vote-recorded"}))
+
+  (route/not-found "Not Found"))
