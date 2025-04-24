4cdb67ae07073c86e093bcc28040ea33ca2aed64
tttTAAAKUUU <tctembedza@gmail.com>
2025-04-24 12:50:22 +0200

    new fleshed out routes, validated inputs

---
+++ backend/src/clondwich/routes.clj
@@ -0,0 +1,51 @@
+(ns clondwich.routes
+  (:require
+   [compojure.core :refer [defroutes GET POST]]
+   [compojure.route :as route]
+   [ring.util.response :refer [response]]
+   [ring.util.request :as req]
+   [db.core :as db]))
+
+(defroutes app-routes
+  ;; Welcome route
+  (GET "/" [] (response "Welcome to the Clondwich App!"))
+
+  ;; Return images ordered by vote count
+  (GET "/images" []
+    (response (db/get-images-by-vote-count db/datasource)))
+
+  ;; Cast a vote on an image
+  (POST "/vote" [image_id vote]
+    (try
+      (let [image-id (Integer/parseInt image_id)
+            vote-bool (= vote "true")]
+        (db/insert-vote db/datasource image-id vote-bool)
+        (response {:status "vote-recorded"}))
+      (catch Exception e
+        (response {:error "Invalid input"}))))
+
+  ;; Insert a new image
+  (POST "/image" [url]
+    (if (and url (not (clojure.string/blank? url)))
+      (do
+        (db/insert-image url)
+        (response {:status "image-added"}))
+      (response {:error "Missing or invalid URL"})))
+
+  ;; Admin routes (used for DB setup/teardown)
+  (POST "/admin/create-tables" []
+    (do
+      (db/create-users-table db/datasource)
+      (db/create-images-table db/datasource)
+      (db/create-votes-table db/datasource)
+      (response {:status "tables-created"})))
+
+  (POST "/admin/drop-tables" []
+    (do
+      (db/drop-users-table db/datasource)
+      (db/drop-images-table db/datasource)
+      (db/drop-votes-table db/datasource)
+      (response {:status "tables-dropped"})))
+
+  ;; Catch-all for undefined routes
+  (route/not-found "Not Found"))
